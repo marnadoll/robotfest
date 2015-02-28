11d83b018d9a00016d2e46d1f1267e3e79a5af93
ali <ali@simpleinformation.com>
2015-02-27 22:39:14 -0500

    continue to create new layout

--- sites/all/themes/robotheme/template.php
+++ sites/all/themes/robotheme/template.php
@@ -5,3 +5,13 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * robotheme theme.
  */
+
+// this is specific to my use case
+// change 'theme' to your theme name, as you would with any
+// preprocess function
+function theme_omega_layout_alter(&$layout) {
+  $cPath = current_path();
+  if (strpos($cPath, 'invite-print') !== 0) {
+    $layout = 'plain';
+  }
+}
